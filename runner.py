5218ee6c4e041023beb68ec4c65c089ef5df6662
Spencer R. Loggia <spencer.loggia98@gmail.com>
2024-05-03 14:10:27 -0400

    update RL runner

--- runner.py
+++ runner.py
@@ -7,14 +7,17 @@ if __name__=="__main__":
     DISJOINT_CRITIC = False
     EPOCHS = 20000
     EPSILON_START = .95
-    EPSILON_DECAY = 2000
+    EPSILON_DECAY = 4000
     ALGORITM = "a3c"
-    LOG_MAX_LR = -7
-    LOG_MIN_LR = -10
+    LOG_MAX_LR = -3.5
+    LOG_MIN_LR = -8
 
     if FC_AGENT:
-        path = "models/fc_evo_32"
+        path = "models/fc_evo_32_bias"
         agent = FCWaterworldAgent(num_nodes=4, spatial=32, channels=3, input_channels=2, device="cpu")
+        # with open("models/wworld_fc_pretrain7.pkl", "rb") as f:
+        #     in_enc = pickle.load(f)
+        # agent.input_encoder = in_enc
     else:
         if DISJOINT_CRITIC:
             path = "models/disjoint_evo_7"
@@ -32,5 +35,5 @@ if __name__=="__main__":
     evo = EvoController(seed_agent=agent, epochs=EPOCHS, num_base=4, num_workers=10,
                         min_agents=1, max_agents=3, min_gen=1, max_gen=1, log_min_lr=LOG_MIN_LR, log_max_lr=LOG_MAX_LR,
                         algo=ALGORITM, start_epsilon=EPSILON_START, inverse_eps_decay=EPSILON_DECAY, worker_device="cpu")
-    # evo.load_model("/Users/loggiasr/Projects/ReIntAI/models/disjoint_evo_7/snap_10500_7.61_.pkl")
+    # evo.load_model("/Users/loggiasr/Projects/ReIntAI/models/fc_evo_32/snap_4500_5.52_.pkl")
     evo.controller(mp=True, fbase=path)
