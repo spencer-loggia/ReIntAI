fba5b801990bbab6093b89f07081f5b7099b5422
Spencer R. Loggia <spencer.loggia98@gmail.com>
2024-05-11 17:25:51 -0400

    add runner to github

--- runner.py
+++ runner.py
@@ -4,26 +4,30 @@ import pickle
 import torch
 torch.set_default_dtype(torch.float64)
 import sys
-if sys.platform == "linux":
-    import torch.multiprocessing as mp
-    mp.set_start_method('spawn', force=True)
 
 if __name__=="__main__":
-    FC_AGENT = True
+    if sys.platform == "linux":
+        import torch.multiprocessing as mp
+        mp.set_start_method('spawn', force=True)
+    FC_AGENT = False
     DISJOINT_CRITIC = False
     EPOCHS = 20000
     EPSILON_START = 1.0
     EPSILON_DECAY = 4000
     ALGORITM = "a3c"
-    LOG_MAX_LR = -4
-    LOG_MIN_LR = -7
+    LOG_MAX_LR = -3
+    LOG_MIN_LR = -8
 
     if FC_AGENT:
-        path = "models/fc_evo_32_bias"
-        agent = FCWaterworldAgent(num_nodes=4, spatial=32, channels=3, input_channels=2, device="cpu")
-        # with open("models/wworld_fc_pretrain7.pkl", "rb") as f:
-        #     in_enc = pickle.load(f)
-        # agent.input_encoder = in_enc
+        if DISJOINT_CRITIC:
+            path = "models/dijoint_fc_evo_32_bias"
+            agent = FCWaterworldAgent(num_nodes=4, spatial=32, channels=3, input_channels=2, device="cpu", decode_node=None)
+            # with open("models/wworld_fc_pretrain7.pkl", "rb") as f:
+            #     in_enc = pickle.load(f)
+            # agent.input_encoder = in_enc
+        else:
+            path = "models/fc_evo_32_bias_no_back"
+            agent = FCWaterworldAgent(num_nodes=4, spatial=32, channels=3, input_channels=2, device="cpu")
     else:
         if DISJOINT_CRITIC:
             path = "models/disjoint_evo_7"
@@ -38,8 +42,8 @@ if __name__=="__main__":
             in_enc = pickle.load(f)
         agent.input_encoder = in_enc.to(torch.float64)
 
-    evo = EvoController(seed_agent=agent, epochs=EPOCHS, num_base=4, num_workers=10,
+    evo = EvoController(seed_agent=agent, epochs=EPOCHS, num_base=1, num_workers=10,
                         min_agents=1, max_agents=3, min_gen=1, max_gen=1, log_min_lr=LOG_MIN_LR, log_max_lr=LOG_MAX_LR,
                         algo=ALGORITM, start_epsilon=EPSILON_START, inverse_eps_decay=EPSILON_DECAY, worker_device="cpu")
-    # evo.load_model("/Users/loggiasr/Projects/ReIntAI/models/fc_evo_32_bias/snap_500_7.85_.pkl")
+    # evo.load_model("/Users/loggiasr/Projects/ReIntAI/models/evo_7/snap_8500_13.59_.pkl")
     evo.controller(mp=True, fbase=path)
